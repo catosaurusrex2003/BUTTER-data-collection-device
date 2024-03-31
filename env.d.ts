cf90e56a7d1fe57514fe0726ad22ba87238a8c27
catosaurusrex2003 <mohdmehdi2003@gmail.com>
2024-03-31 18:28:41 +0530

    env types updated

--- env.d.ts
+++ env.d.ts
@@ -2,9 +2,21 @@
 // Cannot find module '@env' or its corresponding type declarations.ts(2307)
 declare module "@env" {
   export const BASEURL: string;
-  export const TESTENV: string;
+  export const EXPO_PUBLIC_BASEURL: string;
+
   export const EXPO_PUBLIC_GOOGLE_MAPS_APIKEY: string;
+
   export const EXPO_PUBLIC_AWS_region: string;
   export const EXPO_PUBLIC_AWS_accessKeyId: string;
   export const EXPO_PUBLIC_AWS_secretAccessKey: string;
+  export const EXPO_PUBLIC_AWS_bucketName: string;
+  export const EXPO_PUBLIC_AWS_bucketRegion: string;
+  
+  export const EXPO_PUBLIC_apiKey: string;
+  export const EXPO_PUBLIC_authDomain: string;
+  export const EXPO_PUBLIC_projectId: string;
+  export const EXPO_PUBLIC_storageBucket: string;
+  export const EXPO_PUBLIC_messagingSenderId: string;
+  export const EXPO_PUBLIC_appId: string;
+  export const EXPO_PUBLIC_measurementId: string;
 }
